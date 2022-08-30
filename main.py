c85e6211642a98b2ef427e7e1f6598968f24d8bd
Tealalal <1332024415@qq.com>
2022-08-30 11:06:43 +0800

    第二次版本提交

--- main.py
+++ main.py
@@ -10,49 +10,47 @@ import scan_OS
 if __name__ == '__main__':
     start_time = time.time()
     if len(sys.argv) < 2:
-        print("请输入要扫描的主机地址，如：192.168.1.1")
+        print("请在终端输入要扫描的主机地址，如：-i 192.168.1.1或者 -n 192.168.1.0/24")
         sys.exit()
     host = sys.argv[2]  # 在终端输入ip
 
     # 以下注释部分属于获取操作系统版本的nmap，还没调试完成
-    # parser = OptionParser("Usage:%prog -i <target host> -n <network> or -t ttl_scan")  # 输出帮助信息
-    # parser.add_option('-i', type='string', dest='tgtIP', help='specify target host')  # 获取ip地址参数
-    # parser.add_option('-t', type='string', dest='ttl', help='input -t ttl')  # 是否使用ttl模块扫描
-    # parser.add_option('-n', type="string", dest='tgtNetwork', help='specify target host')  # 获取网段信息
-    # options, args = parser.parse_args()
-    # # 实例化用户的参数
-    # tgtip = options.tgtIP
-    # Network = options.tgtNetwork
-    # TTL = options.ttl
-    #
-    # if tgtip is None and Network is None:  # 判断文件是否存在
-    #     print(parser.usage)
-    #     sys.exit()
-    #
-    # if tgtip is not None:  # 单个地址时执行的操作
-    #     if TTL:  # 判断是否调用ttl扫描
-    #         scan_OS.ttl_scan(tgtip)
-    #     else:
-    #         scan_OS.nmap_scan(tgtip)
-    #
-    # if Network:
-    #     prefix = Network.split(".")[0] + '.' + Network.split(".")[1] + '.' + Network.split(".")[2] + '.'  # 以点为分割提取ip
-    #     # 地址前三位
-    #
-    #     if TTL:  # 判断是否调用ttl扫描
-    #         for i in range(1, 255):
-    #             ip = prefix + str(i)  # 结合ip地址前缀生成网段扫描所需ip
-    #             t = threading.Thread(target=scan_OS.ttl_scan, args=(ip,))
-    #             t.start()
-    #             time.sleep(0.1)
-    #     else:
-    #         for i in range(1, 255):
-    #             ip = prefix + str(i)
-    #             t = threading.Thread(target=scan_OS.nmap_scan, args=(ip,))
-    #             t.start()
+    #nmap部分还没完成，ttl可以扫描简单的操作系统，输入参数为-i 192.168.1.1或者 -n 192.168.1.0/24
+    parser = OptionParser("Usage:%prog -i <target host> -n <network> or -t ttl_scan")  # 输出帮助信息
+    parser.add_option('-i', type='string', dest='tgtIP', help='specify target host')  # 获取ip地址参数
+    parser.add_option('-t', type='string', dest='ttl', help='input -t ttl')  # 是否使用ttl模块扫描
+    parser.add_option('-n', type="string", dest='tgtNetwork', help='specify target host')  # 获取网段信息
+    options, args = parser.parse_args()
+    # 实例化用户的参数
+    tgtip = options.tgtIP
+    Network = options.tgtNetwork
+    TTL = options.ttl
 
+    if tgtip is None and Network is None:  # 判断文件是否存在
+        print(parser.usage)
+        sys.exit()
 
+    if tgtip is not None:  # 单个地址时执行的操作
+        if TTL:  # 判断是否调用ttl扫描
+            scan_OS.ttl_scan(tgtip)
+        else:
+            scan_OS.nmap_scan(tgtip)
 
+    if Network:
+        prefix = Network.split(".")[0] + '.' + Network.split(".")[1] + '.' + Network.split(".")[2] + '.'  # 以点为分割提取ip
+        # 地址前三位
+
+        if TTL:  # 判断是否调用ttl扫描
+            for i in range(1, 255):
+                ip = prefix + str(i)  # 结合ip地址前缀生成网段扫描所需ip
+                t = threading.Thread(target=scan_OS.ttl_scan, args=(ip,))
+                t.start()
+                time.sleep(0.1)
+        else:
+            for i in range(1, 255):
+                ip = prefix + str(i)
+                t = threading.Thread(target=scan_OS.nmap_scan, args=(ip,))
+                t.start()
 
     thread_list = []
 
--- scan_OS.py
+++ scan_OS.py
@@ -7,19 +7,25 @@ from optparse import OptionParser
 from threading import Thread
 from scapy.all import *
 from scapy.layers.inet import ICMP
+import logging
 
 
 def ttl_scan(ip):#简易扫描
-    packet = IP(dst=ip) / ICMP()
-    result = sr1(packet, timeout=1, verbose=0)
-    if result is None:
+    try:
+        packet = IP(dst=ip) / ICMP()
+        result = sr1(packet, timeout=1, verbose=0)
+
+        if result is None:
+            logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+            pass
+
+        elif int(result[IP].ttl) <= 64:  # 判断目标主机响应包中TTL值是否小于等于64
+            print("%s  is Linux/Unix" % ip)  # 是的话就为linux/Unix
+
+        else:
+            print("%s is Windows" % ip)  # 反之就是windows
+    except:
         pass
-
-    elif int(result[IP].ttl) <= 64:  # 判断目标主机响应包中TTL值是否小于等于64
-        print("%s  is Linux/Unix" % ip)  # 是的话就为linux/Unix
-
-    else:
-        print("%s is Windwows" % ip)  # 反之就是linux
 
 
 def nmap_scan(ip):#复杂扫描nmap
