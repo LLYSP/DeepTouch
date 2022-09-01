acb583990be0626effdfb5116f89bf1d1425ac0e
Tealalal <92720453+Tealalal@users.noreply.github.com>
2022-09-01 17:45:31 +0800

    Add files via upload

--- scan_host.py
+++ scan_host.py
@@ -13,11 +13,26 @@ def ttl_scan(ip,ip_list):  # 探测ip
             pass
 
         elif int(result[IP].ttl) <= 64:  # 判断目标主机响应包中TTL值是否小于等于64
-            print("%s  is Linux/Unix" % ip)  # 是的话就为linux/Unix
-            ip_list.append(ip)
+            print("%s\tis Linux/Unix" % ip)  # 是的话就为linux/Unix
 
         else:
-            print("%s is Windows" % ip)  # 反之就是windows
+            print("%s\tis Windows" % ip)  # 反之就是windows
+
+    except:
+        pass
+
+
+def ttl_scan_noshow(ip, ip_list):  # 探测ip
+    try:
+        packet = IP(dst=ip) / ICMP()
+        result = sr1(packet, timeout=1, verbose=0)
+        if result is None:
+            logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+            pass
+
+        elif int(result[IP].ttl) <= 64:# 判断目标主机响应包中TTL值是否小于等于64
+            ip_list.append(ip)
+        else:
             ip_list.append(ip)
     except:
         pass
